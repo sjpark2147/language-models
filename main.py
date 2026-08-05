fe805b1bcbeb1d6a5dac81456f760a1f011d3187
Sejun Park <me@safespot.dev>
2026-08-05 11:47:17 +0900

    Init uv environment

---
+++ main.py
@@ -0,0 +1,10 @@
+from lmproj.core.module import test
+
+def main():
+    print("Hello from language-models!")
+    test()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/lmproj/core/module.py
@@ -0,0 +1,4 @@
+
+
+def test():
+    print("module test function")
